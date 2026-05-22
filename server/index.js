891ddcd01f686795fe80ef31e58ea911e0b0c491
Saransh Rathore <Admin.BloodBridge@gmail.com>
2026-05-22 20:41:31 +0530

    Fix: Use Atlas MongoDB for production

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use('/api/requests', requestRoutes);
 const otpRoutes = require('./routes/otpRoutes');
 app.use('/api/otp', otpRoutes);
 
-const MONGO_URI = "mongodb://localhost:27017/bloodbridge";
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, {
   serverSelectionTimeoutMS: 5000,
