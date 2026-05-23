d5cfd8e131a54d554c9a6d08991d280787190824
Saransh Rathore <Admin.BloodBridge@gmail.com>
2026-05-23 21:52:44 +0530

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -7,10 +7,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(helmet());
-app.use(cors({
-  origin: '*',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 // Routes
 const authRoutes = require('./routes/authRoutes');
